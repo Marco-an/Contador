ee6c2d2bb496f9852f90394d84baf5ae33159c57
leandro <leandrosif3@gmail.com>
2023-02-19 18:23:33 -0500

    Commit de contador de javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+const vPantalla = document.getElementById('pantalla');
+const btnReducir = document.getElementById('reducir');
+const btnReiniciar = document.getElementById('reiniciar');
+const btnAumentar = document.getElementById('aumentar');
+
+let contador = 0;
+btnAumentar.onclick = function () {
+  contador++;
+  if (contador > 0) {
+    vPantalla.style = 'color:green;';
+  } else if (contador == 0) {
+    vPantalla.style = 'color:blue;';
+  }
+  vPantalla.innerText = contador;
+}
+
+btnReiniciar.onclick = function () {
+  contador = 0;
+  vPantalla.style = 'color:blue;';
+  vPantalla.innerText = contador;
+}
+
+btnReducir.onclick = function () {
+  contador--;
+  if (contador < 0) {
+    vPantalla.style = 'color:red;';
+  } else if (contador == 0) {
+    vPantalla.style = 'color:blue;';
+  }
+  vPantalla.innerText = contador;
+}
+
